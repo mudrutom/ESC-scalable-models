fa899e5f4f0a0bc8f6ade6e397963449041370ce
Tomáš Mudruňka <mudrutom@gmail.com>
2014-08-18 20:06:36 +0200

    added support for parsing ARFF input data format

--- src/main/scala/cz/cvut/esc/models/InputDataParser.scala
+++ src/main/scala/cz/cvut/esc/models/InputDataParser.scala
@@ -1,6 +1,7 @@
 package cz.cvut.esc.models
 
 import org.apache.spark.SparkContext
+import org.apache.spark.mllib.linalg.DenseVector
 import org.apache.spark.mllib.regression.LabeledPoint
 import org.apache.spark.mllib.util.MLUtils
 import org.apache.spark.rdd.RDD
@@ -8,7 +9,7 @@ import org.apache.spark.rdd.RDD
 /** Input data format enumeration. */
 object InputFormat extends Enumeration {
 	type InputFormat = Value
-	val SVM, LP = Value
+	val SVM, LP, ARFF = Value
 }
 
 import cz.cvut.esc.models.InputFormat._
@@ -36,13 +37,49 @@ trait InputDataParser[P <: Params] {
 	 */
 	def parseAndSplitData(sc: SparkContext, params: P): (RDD[LabeledPoint], RDD[LabeledPoint]) = {
 		// parse the input data
-		val data = params.inputFormat match {
-			case SVM => MLUtils.loadLibSVMFile(sc, params.input)
-			case LP => MLUtils.loadLabeledData(sc, params.input)
-		}
+		val data = parseData(sc, params)
 
 		// split data into training and test sets
 		val splits = data.randomSplit(Array(params.trainSplit, 1.0 - params.trainSplit), seed = seed)
 		(splits(0), splits(1))
+	}
+
+	/**
+	 * Parse the input data.
+	 * @param sc Spark context
+	 * @param params input parameters
+	 * @return parsed data
+	 */
+	def parseData(sc: SparkContext, params: P): RDD[LabeledPoint] = {
+		params.inputFormat match {
+			case SVM => MLUtils.loadLibSVMFile(sc, params.input)
+			case LP => MLUtils.loadLabeledData(sc, params.input)
+			case ARFF => parseARFF(sc, params.input)
+		}
+	}
+
+	/**
+	 * Parsing of the Attribute-Relation File Format (ARFF).
+	 * @param sc Spark context
+	 * @param path file or directory path
+	 * @return parsed data
+	 */
+	private def parseARFF(sc: SparkContext, path: String) = {
+  	val file = sc.textFile(path)
+
+		// parse header with attributes info
+		val attrs = file.filter(_.startsWith("@attribute")).map(_.substring(10).trim).collect()
+		val parsers = attrs.map {
+			case attr if attr.endsWith("numeric") => (value: String) => value.toDouble
+			case attr if attr.matches("^.*[{].*[}]$") =>
+				val nominals = attr.substring(attr.indexOf('{') + 1, attr.indexOf('}')).split(",")
+				(value: String) => nominals.indexOf(value).toDouble
+			case _ => (_: String) => Double.NaN
+		}
+		val last = sc.broadcast(attrs.length - 1)
+
+		val lines = file.filter(line => !line.isEmpty && !line.startsWith("%") && !line.startsWith("@")).map(_.split(",").map(_.trim))
+		val rows = lines.map(_.zip(parsers).map { case (value, parser) => parser(value) })
+		rows.map(row => new LabeledPoint(row(last.value), new DenseVector(row.slice(0, last.value))))
 	}
 }
