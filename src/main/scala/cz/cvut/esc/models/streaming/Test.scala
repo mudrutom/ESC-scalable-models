dd6f49ae0da918b55c52e6c23e94f0b8367554ac
Tomáš Mudruňka <mudrutom@gmail.com>
2014-08-20 14:58:04 +0200

    testing of the updateStateByKey method

--- src/main/scala/cz/cvut/esc/models/streaming/Test.scala
+++ src/main/scala/cz/cvut/esc/models/streaming/Test.scala
@@ -5,10 +5,13 @@ import cz.cvut.esc.models.classifiers.{ParamsSVM, SparkSVM}
 import cz.cvut.esc.models.{CliApp, InputDataParser, InputFormat, Params}
 import org.apache.log4j.{Level, Logger}
 import org.apache.spark.SparkContext._
+import org.apache.spark.mllib.classification.ClassificationModel
+import org.apache.spark.mllib.evaluation.BinaryClassificationMetrics
 import org.apache.spark.mllib.linalg.DenseVector
 import org.apache.spark.mllib.regression.LabeledPoint
 import org.apache.spark.rdd.RDD
-import org.apache.spark.streaming.{Milliseconds, StreamingContext}
+import org.apache.spark.streaming.StreamingContext._
+import org.apache.spark.streaming.{Seconds, StreamingContext}
 import org.apache.spark.{SparkConf, SparkContext}
 import scopt.OptionParser
 
@@ -19,6 +22,12 @@ case class ParamsT(
 										inputFormat: InputFormat = SVM,
 										trainSplit: Double = 0.6
 										) extends Params
+
+case class TestState(
+											best: Option[ClassificationModel] = None,
+											bestAuPR: Double = 0.0,
+											current: ClassificationModel
+											)
 
 object Test extends CliApp[ParamsT] with InputDataParser[ParamsT] {
 
@@ -35,10 +44,11 @@ object Test extends CliApp[ParamsT] with InputDataParser[ParamsT] {
 		(parser, new ParamsT())
 	}
 
+	val sc = new SparkContext(new SparkConf())
+
 	override def run(params: ParamsT) {
 		Logger.getRootLogger.setLevel(Level.WARN)
 
-		val sc = new SparkContext(new SparkConf())
 		val data = parseData(sc, params)
 
 		// the first value is a timestamp
@@ -49,23 +59,49 @@ object Test extends CliApp[ParamsT] with InputDataParser[ParamsT] {
 
 		// split data into batches
 		val sequence = seqData.keys.distinct().collect()
-		val q = new mutable.Queue[RDD[LabeledPoint]]()
-		for (s <- sequence) q.enqueue(seqData.filter(_._1 == s).values)
+		val q = new mutable.Queue[RDD[(Double, LabeledPoint)]]()
+		for (s <- sequence) q.enqueue(seqData.filter(_._1 == s))
 
 		// init stream
-		val ssc = new StreamingContext(sc, Milliseconds(100L))
-		val stream = ssc.queueStream(q).window(Milliseconds(500L), Milliseconds(500L))
+		val ssc = new StreamingContext(sc, Seconds(1L))
+		ssc.checkpoint("temp")
+		val stream = ssc.queueStream(q)
 
-		// do the SVM training
-		stream.foreachRDD(rdd => {
-			val t = System.currentTimeMillis()
-			val model = SparkSVM.trainClassifier(rdd.cache(), ParamsSVM())
-			println(model.toString + " " + (System.currentTimeMillis() - t))
-		})
+		def updateState(points: Seq[LabeledPoint], state: Option[TestState]): Option[TestState] = {
+			if (points.size < 1) None
+			else {
+				// do the SVM training
+				val data = sc.makeRDD(points).cache()
+				val model = SparkSVM.trainClassifier(data, ParamsSVM())
+
+				// update the state
+				Some(state match {
+					case None => TestState(current = model)
+					case Some(s) =>
+						val auPR = computeAuPR(Some(s.current), data)
+						val bestAuPR = computeAuPR(s.best, data)
+						println(bestAuPR + " curr=" + auPR)
+						if (auPR > bestAuPR) TestState(current = model, best = Some(s.current), bestAuPR = auPR)
+						else s.copy(current = model, bestAuPR = bestAuPR)
+				})
+			}
+		}
+
+		stream.updateStateByKey(updateState).foreachRDD(rdd => println(rdd.count()))
 
 		// start processing
 		ssc.start()
 		ssc.awaitTermination()
+	}
+
+	def computeAuPR(model: Option[ClassificationModel], data: RDD[LabeledPoint]) = {
+		model match {
+			case None => 0.0
+			case Some(m) =>
+				val prediction = m.predict(data.map(_.features)).zip(data.map(_.label))
+				val metrics = new BinaryClassificationMetrics(prediction)
+				metrics.areaUnderPR()
+		}
 	}
 
 }
